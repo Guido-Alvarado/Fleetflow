ec38c42a1b4b58ad6a128029910870ea2ad74be2
Guido <guidoalvarado10@gmail.com>
2025-11-30 21:42:21 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Fleetflow/sw.js', { scope: '/Fleetflow/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
